fa40fe94c6609a6d201e933112bf173147da90c3
Luwoye Kayode <luwoyekayode99@gmail.com>
2023-01-04 05:56:39 -0800

    task8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[34:59]
+str = str[43:70]
 print(str)
